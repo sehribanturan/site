1f6c8f2c8b805a536bb248037abc44da6622bdc7
Şehriban Turan <107572130+sehribanturan@users.noreply.github.com>
2023-04-11 17:23:53 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <Anasayfa />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Anasayfa from "../components/Anasayfa.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    Anasayfa,
+  },
+};
+</script>
