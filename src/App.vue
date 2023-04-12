a38fcc8a0e770ad8a288efc7fe605c396394cde1
Şehriban Turan <107572130+sehribanturan@users.noreply.github.com>
2023-04-13 00:00:18 +0300

    anasayfa

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,77 @@
 <template>
-  <nav>
-    <router-link to="/">Anasayfa</router-link> |
-    <router-link to="/Product">Ürünlerimiz</router-link> |
-    <router-link to="/about">Hakkımızda</router-link> |
-    <router-link to="/login">Giriş Yap</router-link> |
-    <router-link to="/register">Kayıt ol</router-link> |
-    <router-link to="/contact">İletişim</router-link> |
-    <router-link to="/pay">Ödemeler</router-link> |
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand">
+      <router-link to="/">Anasayfa</router-link>
+      <router-link to="/Product">Ürünlerimiz</router-link>
+      <router-link to="/about">Hakkımızda</router-link>
+      <router-link to="/contact">İletişim</router-link>
+
+      <form class="form-inline">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
+      <router-link to="/register">Kayıt ol</router-link>
+      <router-link to="/login">Giriş Yap</router-link>
+      <router-link to="/pay">
+        <div class="menu">
+          <font-awesome-icon icon="shopping-cart" />
+          <span class="cart-count">{{ cartItemCount }}</span>
+        </div>
+      </router-link>
+    </a>
   </nav>
-  <router-view />
+
+  <router-view> </router-view>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      cartItemCount: 0,
+    };
+  },
+  methods: {
+    addToCart() {
+      // Sepete ürün ekleme kodu burada
+      this.cartItemCount += 1;
+    },
+  },
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+a {
+  color: #42b983;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.navbar-brand a {
+  text-decoration: none;
+  padding: 2px;
+}
+.navbar-brand a:hover {
+  background: #345f4a;
+  color: white;
+}
+.navbar-brand {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  font-size: 18px;
+}
+.form-control {
+  margin-left: 10px;
 }
 </style>
--- src/components/Anasayfa.vue
+++ src/components/Anasayfa.vue
@@ -1,6 +1,383 @@
 <template>
-  <div class="">
-    <h1>Anasayfa</h1>
+  <div class="Anasayfa">
+    <main role="main" class="main">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- slider geçişlerinin oldugu küçük çizgiler(li) -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+          <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+          <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+          <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+          <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+        </ol>
+        <!-- slider yazılarının her biri(carousel-item) -->
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <svg
+              class="bd-placeholder-img"
+              width="100%"
+              height="100%"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+              role="img"
+            >
+              <rect width="100%" height="100%" fill="#42b983"></rect>
+            </svg>
+
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Kaliteli Ürün.</h1>
+                <p>Ürünlerimiz kaliteli malzemelerden üretilir.</p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <svg
+              class="bd-placeholder-img"
+              width="100%"
+              height="100%"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+              role="img"
+            >
+              <rect width="100%" height="100%" fill="#42b983"></rect>
+            </svg>
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Güvenli Alışveriş</h1>
+                <p>Güvenli satın alma seçenekleri ile hizmetinizdeyiz.</p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <svg
+              class="bd-placeholder-img"
+              width="100%"
+              height="100%"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+              role="img"
+            >
+              <rect width="100%" height="100%" fill="#42b983"></rect>
+            </svg>
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Hızlı Teslimat</h1>
+                <p>Kargonuz 3 iş günü içerisinde güvenle teslim edilir.</p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <svg
+              class="bd-placeholder-img"
+              width="100%"
+              height="100%"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+              role="img"
+            >
+              <rect width="100%" height="100%" fill="#42b983"></rect>
+            </svg>
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Özenli paketleme</h1>
+                <p>Kargolarınız özenle paketlenir</p>
+                <p></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <svg
+              class="bd-placeholder-img"
+              width="100%"
+              height="100%"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+              role="img"
+            >
+              <rect width="100%" height="100%" fill="#42b983"></rect>
+            </svg>
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Müşteri memnuniyeti</h1>
+                <p>Müşterilerimizin memnuniyeti için buradayız.</p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <svg
+              class="bd-placeholder-img"
+              width="100%"
+              height="100%"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+              role="img"
+            >
+              <rect width="100%" height="100%" fill="#42b983"></rect>
+            </svg>
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Hesaplı Fiyatlar</h1>
+                <p>Ürünlerimiz hesaplı ve kalıtelidir</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- sağ geri simgesi -->
+        <a
+          class="carousel-control-prev"
+          href="#myCarousel"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <!-- sağ geri simgesi -->
+        <a
+          class="carousel-control-next"
+          href="#myCarousel"
+          role="button"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+      <div class="container marketing">
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+          <div class="col-lg-4">
+            <a href="#">
+              <h5 class="card-title">Gümüş Kolye Çeşitleri</h5>
+              <div class="card" style="width: 13rem">
+                <img src="./images/kelebek.jpeg" class="card-img-top" />
+                <div class="card-body">
+                  <p class="card-text"></p>
+                </div>
+              </div>
+            </a>
+          </div>
+          <!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <a href="#">
+              <h5 class="card-title">Tesbih Türleri</h5>
+              <div class="card" style="width: 13rem">
+                <img src="./images/tespih.jpeg" class="card-img-top" />
+                <div class="card-body">
+                  <p class="card-text">Gümüş Saat,</p>
+                </div>
+              </div>
+            </a>
+          </div>
+          <!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <a href="#">
+              <h5 class="card-title">İmitasyon Takılar</h5>
+              <div class="card" style="width: 13rem">
+                <img src="./images/imitasyon.jpeg" class="card-img-top" />
+                <div class="card-body">
+                  <p class="card-text">Gümüş Saat,</p>
+                </div>
+              </div>
+            </a>
+          </div>
+          <!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <a href="#">
+              <h5 class="card-title">Saat Çeşitleri</h5>
+              <div class="card" style="width: 13rem">
+                <img src="./images/saat.jpeg" class="card-img-top" />
+                <div class="card-body">
+                  <p class="card-text">Gümüş Saat,</p>
+                </div>
+              </div>
+            </a>
+          </div>
+          <!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <a href="#">
+              <h5 class="card-title">Dogal Taş Ürünler</h5>
+              <div class="card" style="width: 13rem">
+                <img src="./images/dgltas.jpeg" class="card-img-top" />
+                <div class="card-body">
+                  <p class="card-text">Kolye,Bilezik</p>
+                </div>
+              </div>
+            </a>
+          </div>
+          <!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <a href="#">
+              <h5 class="card-title">Hediyelik Eşyalar</h5>
+              <div class="card" style="width: 13rem">
+                <img src="./images/images.jpeg" class="card-img-top" />
+                <div class="card-body">
+                  <p class="card-text">magnet</p>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- START THE FEATURETTES -->
+
+        <hr class="featurette-divider" />
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">
+              <a href="">Kadına Alınabilecek Hediye Önerileri</a>
+            </h2>
+            <p class="lead">zarif...</p>
+          </div>
+          <div class="col-md-5">
+            <a href=""> <img class="hdye" src="./images/kdn.jpeg" alt="" /></a>
+          </div>
+        </div>
+
+        <hr class="featurette-divider" />
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2">
+            <a href=""
+              ><h2 class="featurette-heading">
+                Erkekler için Hediye Önerileri
+              </h2></a
+            >
+            <p class="lead">cömert...</p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <a href=""><img class="hdye" src="./images/ekek.jpeg" alt="" /></a>
+          </div>
+        </div>
+
+        <hr class="featurette-divider" />
+
+        <!-- /END THE FEATURETTES -->
+      </div>
+      <!-- /.container -->
+
+      <!-- FOOTER -->
+      <footer class="container">
+        <div class="inner">
+          <div class="col col-3 col-md-4 col-sm-12">
+            <ul class="fl col-12">
+              <li class="col-sm-12">
+                <h3 class="ease">
+                  Önemli Bilgiler
+                  <span class="fr icon-wrapper forMobile">
+                    <i class="icon-plus"></i>
+                    <i class="icon-minus d-none"></i>
+                  </span>
+                </h3>
+                <div class="fl col-12">
+                  <ul class="fl col-12">
+                    <li class="col-sm-12">
+                      <a href="/teslimat-kosullari">Teslimat Koşulları</a>
+                    </li>
+                    <li class="col-sm-12">
+                      <a href="/uyelik-sozlesmesi">Üyelik Sözleşmesi</a>
+                    </li>
+                    <li class="col-sm-12">
+                      <a href="/satis-sozlesmesi">Satış Sözleşmesi</a>
+                    </li>
+                    <li class="col-sm-12">
+                      <a href="/garanti-ve-iade-kosullari"
+                        >Garanti ve İade Koşulları</a
+                      >
+                    </li>
+                    <li class="col-sm-12">
+                      <a href="/gizlilik-ve-guvenlik">Gizlilik ve Güvenlik</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="col col-3 col-md-4 col-sm-12">
+            <ul class="fl col-12">
+              <li class="col-sm-12">
+                <h3 class="ease">
+                  Hızlı Erişim
+                  <span class="fr icon-wrapper forMobile">
+                    <i class="icon-plus"></i>
+                    <i class="icon-minus d-none"></i>
+                  </span>
+                </h3>
+                <div class="fl col-12">
+                  <ul class="fl col-12">
+                    <li class="col-sm-12">
+                      <a href="/">Anasayfa</a>
+                    </li>
+                    <li class="col-sm-12">
+                      <a href="/yeni-urunler">Yeni Ürünler</a>
+                    </li>
+                    <li class="col-sm-12">
+                      <a href="/indirimli-urunler">İndirimdekiler</a>
+                    </li>
+                    <li class="col-sm-12">
+                      <a href="/musteri-hizmetleri">Müşteri Hizmetleri</a>
+                    </li>
+                    <li class="col-sm-12">
+                      <a href="/sepet">Sepetim</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="col col-3 col-sm-12" id="footerContact">
+            <h3 class="ease">
+              Hakkımızda
+              <span class="fr icon-wrapper forMobile">
+                <i class="icon-plus"></i>
+                <i class="icon-minus d-none"></i>
+              </span>
+            </h3>
+            <div class="fl col-12" id="contactInfo">
+              <p>
+                <strong class="fw700">
+                  <i class="icon-phone text-custom-pink text-center"></i>
+                  Telefon:
+                </strong>
+                +90 212 00 00 00 00
+              </p>
+              <p>
+                <strong class="fw700">
+                  <i class="icon-email text-custom-pink text-center"></i>
+                  E-posta:
+                </strong>
+                info@ornek.com
+              </p>
+              <p>
+                <strong class="fw700">
+                  <i class="icon-pointer text-custom-pink text-center"></i>
+                  Adres </strong
+                ><br />
+
+                Pendik/ İstanbul
+              </p>
+            </div>
+          </div>
+        </div>
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>
+          © 2023-2024 Company, Inc. · <a href="#">Privacy</a> ·
+          <a href="#">Terms</a>
+        </p>
+      </footer>
+    </main>
+    <b-button variant="success"></b-button>
   </div>
 </template>
   
@@ -15,16 +392,53 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
+  text-decoration: none;
+  font-family: "Noto Serif", serif;
+  font-family: "Orbitron", sans-serif;
+  font-family: "Roboto", sans-serif;
+}
+a:hover {
+  color: rgb(10, 92, 33);
+}
+
+.img {
+  width: 200px;
+  height: 200px;
+  margin-top: 20px;
+  border-radius: 430px;
+}
+.hdye {
+  width: 400px;
+}
+.carousel-inner {
+  text-align: right;
+  display: flex;
+}
+.carousel-item {
+}
+.inner {
+  display: flex;
+}
+.inner ul li {
+  text-decoration: none;
+  list-style: none;
+  color: black;
+}
+.inner {
+  display: flex;
+}
+.inner ul li {
+  text-decoration: none;
+  list-style: none;
+}
+.inner a {
+  color: black;
+}
+.ease {
+  align-items: center;
 }
 </style>
   
